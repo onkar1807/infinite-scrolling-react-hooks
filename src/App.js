f9a0cd6af338952905a904dbbe26d583c5fab871
Onkar Magdum <onkarm533@gmail.com>
2021-10-12 14:23:25 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
+import loading from './loading.35b947f5.gif';
 import './App.css';
 
 function App() {
+
+  const [photos, setPhotos] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [isloading, setIsLoading] = useState(false);
+
+  const fetchPhotos = async(pageNumber) => {
+    const access_key = "jQjPThuobtuvBg0YMT3jH-eDKjRPLaP8094CCWZPFOA"
+    const res = await fetch(
+      `https://api.unsplash.com/photos/?client_id=${access_key}&page=${pageNumber}&per_page=10`
+      )
+
+    const data = await res.json();
+    setPhotos(photo => [...photo, ...data]);
+    setIsLoading(true)
+  }
+
+  useEffect(() => {
+    fetchPhotos(pageNumber)
+  },[pageNumber])
+
+
+  const loadMore = () => {
+    setPageNumber(prevPage => prevPage + 1)
+  }
+
+
+  const pageEnd = useRef();
+  let num = 1;
+
+  useEffect(() => {
+    if(isloading) {
+      const observer = new IntersectionObserver(entries => {
+
+        if(entries[0].isIntersecting) {
+          num++;
+          loadMore();
+          if(num >= 10) {
+            observer.unobserve(pageEnd.current)
+          }
+        }
+      },
+      {threshold: true});
+      
+      observer.observe(pageEnd.current);
+    }
+  },[isloading,num])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Infinite Scrolling React Hooks</h1>
+      {
+        photos && photos.map((photo, idx) => (
+          <div className="photos" key={idx}>
+            <img src={photo.urls.small} alt="#" />
+            <p>{photo.user.first_name + ' ' + photo.user.last_name}</p>
+            <span>Likes: {photo.user.total_likes}</span>
+          </div>
+        ))
+      }
+
+      <div className="loading">
+        <img src={loading} alt="#" />
+      </div>
+
+      <h3>{photos.length}</h3>
+
+      <button 
+        ref={pageEnd}
+        onClick={loadMore}
+      >
+        Load More
+      </button>
     </div>
   );
 }
